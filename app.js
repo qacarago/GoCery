2ff5ebc1c6783793bfa3ae5e38c1bc381c5e3809
qacarago <qacarago@tip.edu.ph>
2025-11-08 18:51:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+
+// Sidebar Button
 const sidebar = document.getElementById('sidebar')
 
 function toggleSidebar(){
     sidebar.classList.toggle("show")
 }
 
+
+// Sidebar Links
 let home_page = document.querySelector('#home-page');
 let snacks_page = document.querySelector('#snacks-page');
 let beverages_page = document.querySelector('#beverages-page');
@@ -48,4 +52,33 @@ function snacks () {
     ready_to_eat_page.style.display = 'none';
     beverages_page.style.display = 'none';
     snacks_page.style.display = 'block';
+}
+
+
+// Modal Pop Ups
+const modal = document.getElementById("productModal");
+
+function openModal(name, imageSrc, price, description, weight) {
+    document.getElementById("modalProductName").textContent = name;
+    document.getElementById("modalProductImage").src = imageSrc;
+    document.getElementById("modalProductImage").alt = name;
+    document.getElementById("modalProductPrice").textContent = price;
+    document.getElementById("modalProductDescription").textContent = description;
+    document.getElementById("modalProductWeight").textContent = weight;
+
+    modal.style.display = "block";
+    
+    document.body.classList.add('modal-open');
+}
+
+function closeModal() {
+    modal.style.display = "none";
+    
+    document.body.classList.remove('modal-open');
+}
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        closeModal();
+    }
 }
